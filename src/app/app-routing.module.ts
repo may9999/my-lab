15f8eff601e3e8cc1ca838f2c39644de2973f110
Ismael Ruiz Moya <ing_ismael_ruiz@hotmail.com>
2024-03-08 21:29:18 -0600

    comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'home', component: HomeComponent },
-  // { path: 'calendar', component: CalendarComponent },
+  // { path: 'calendar', component: CalendarComponent, canActivate: [AuthGuard] },
   // { path: 'physical-health', component: PhysicalHealthComponent },
   // { path: 'nutritional-health', component: NutritionalHealthComponent },
   // { path: 'conf', component: ConfigurationComponent },
