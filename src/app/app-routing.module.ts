03a7ce58e540daa2b18f684354be80eaa8638beb
Ismael Ruiz Moya <ing_ismael_ruiz@hotmail.com>
2024-03-22 12:53:47 -0600

    adminGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LandingComponent } from './components/landing/landing.component';
-import { AuthGuard } from './auth/guards/auth.guard';
+// import { AuthGuard } from './auth/guards/auth.guard';
 import { UserComponent } from './components/users/user.component';
+import { IsAdminGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
   { path: 'landing', component: LandingComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'user', component: UserComponent, canActivate: [AuthGuard] },
+  // { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  // { path: 'user', component: UserComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [IsAdminGuard] },
+  { path: 'user', component: UserComponent, canActivate: [IsAdminGuard] },
   // { path: 'physical-health', component: PhysicalHealthComponent },
   // { path: 'nutritional-health', component: NutritionalHealthComponent },
   // { path: 'conf', component: ConfigurationComponent },
--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,5 +1,5 @@
-import { Injectable, inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { Inject, Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChildFn, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -21,19 +21,34 @@ import { AuthService } from '../services/auth.service';
 //   return inject(isAdminGuard).canActivate(route, state);
 // }
 
-export class AuthGuard implements CanActivate {
+// export class AuthGuard implements CanActivate {
+
+//   constructor(private authService: AuthService, private router: Router) { }
+
+//   canActivate() {
+//     return this.authService.isLoggedIn();
+//     // if (this.authService.isLoggedIn()) {
+//     //   // return this.router.navigate(['/']);
+//     //   return true;
+//     // } else {
+//     //   // return this.router.navigate(['/landing']);
+//     //   return this.authService.isLoggedIn();
+//     // }
+//     // // return !this.authService.isLoggedIn();
+//   }
+
+class AuthGuard {
 
   constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
     return this.authService.isLoggedIn();
-    // if (this.authService.isLoggedIn()) {
-    //   // return this.router.navigate(['/']);
-    //   return true;
-    // } else {
-    //   // return this.router.navigate(['/landing']);
-    //   return this.authService.isLoggedIn();
-    // }
-    // // return !this.authService.isLoggedIn();
   }
+}
+export const IsAdminGuard: CanActivateChildFn = (route: ActivatedRouteSnapshot,
+state: RouterStateSnapshot): boolean => {
+  return inject(AuthGuard).canActivate(route, state);
 }
